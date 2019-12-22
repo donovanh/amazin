deec9022e1fe21ccaa4d00250a2800af8c012fd2
Donovan Hutchinson <donovanh@gmail.com>
2019-12-22 11:13:06 +0000

    Switching polka to express

--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
 import sirv from 'sirv';
-import polka from 'polka';
+import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-
-console.log('imports done')
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const app = polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
+const app = express()
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
   )
-  
-console.log('PORT is', PORT)
+  .listen(PORT, err => {
+    if (err) console.log('error', err)
+  });
 
-app.listen(PORT, err => {
-  if (err) console.log('error', err);
-})
-  
-export default app.handler
+  export default app;
