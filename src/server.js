2daac502a27523a0a8307280b339034c8b97f931
Donovan Hutchinson <donovanh@gmail.com>
2019-12-23 10:00:57 +0000

    Adjusting server.js

--- src/server.js
+++ src/server.js
@@ -11,9 +11,12 @@ const app = express()
     compression({ threshold: 0 }),
     sirv('static', { dev }),
     sapper.middleware()
-  )
-  .listen(PORT, err => {
-    if (err) console.log('error', err)
-  });
+  );
 
+	if (!process.env.NOW_REGION) {
+		app.listen(PORT, err => {
+			if (err) console.log('error', err)
+		});
+	}
+	
   export default app;
